23454b87197f16c97f081724e8262df16e28e92b
LufeMonster <lucasmodolo2@gmail.com>
2025-03-31 16:36:04 -0300

    Version 0.2
    Corrected the acceleration calculation, removed lots of magic numbers and organized the projectile parameters and methods in the class Projectile.

--- src/simulation.py
+++ src/simulation.py
@@ -5,17 +5,38 @@ import math
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+
+class Projectile:
+    def __init__(self, pos_x, pos_y, vel_x, vel_y, acc_x, acc_y, size, color):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.vel_x = vel_x
+        self.vel_y = vel_y
+        self.acc_x = acc_x
+        self.acc_y = acc_y
+        self.size = size
+        self.color = color
+    
+    def move(self, dist_x, dist_y):
+        self.pos_x += dist_x
+        self.pos_y += dist_y
+    
+    def accel(self, delta_vel_x, delta_vel_y):
+        self.vel_x += delta_vel_x
+        self.vel_y += delta_vel_y
+    
+    def update(self):
+        self.move(self.vel_x, self.vel_y)
+        self.accel(self.acc_x, self.acc_y)
+        
+    def simulate(self, center_x, center_y, gravity_module):
+        self.accel((gravity_module * ((self.pos_x - center_x) / math.sqrt((self.pos_x - center_x) ** 2 + (self.pos_y - center_y) ** 2))) * -1, (gravity_module *((self.pos_y - center_y) / math.sqrt((self.pos_x - center_x) ** 2 + (self.pos_y - center_y) ** 2))) * -1)
+        self.update()
+        pygame.draw.circle(screen, self.color, (self.pos_x, self.pos_y), self.size)
+    
+projectile = Projectile(640, 180, 0.5, 0, 0, 0, 32, "red")
+
 running = True
-
-projectile_pos_x = 640
-projectile_pos_y = 180
-
-projectile_vel_x = 0.5
-projectile_vel_y = 0
-
-projectile_acc_x = 0
-projectile_acc_y = 0
-
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -28,21 +49,13 @@ while running:
 
     # RENDER YOUR GAME HERE
     
-    pygame.draw.circle(screen, (0, 0, 255), (640, 360), 8)
+    pygame.draw.circle(screen, "blue", (640, 360), 8)
     
-    pygame.draw.circle(screen, (255, 0, 0), (projectile_pos_x, projectile_pos_y), 32)
-    projectile_pos_x += projectile_vel_x
-    projectile_pos_y += projectile_vel_y
-    
-    projectile_vel_x += projectile_acc_x
-    projectile_vel_y += projectile_acc_y
-    
-    projectile_acc_x = -0.001 * ((projectile_pos_x - 640) / math.sqrt((projectile_pos_x - 640) ** 2 + (projectile_pos_y - 360) ** 2))
-    projectile_acc_y = -0.001 * ((projectile_pos_y - 360) / math.sqrt((projectile_pos_x - 640) ** 2 + (projectile_pos_y - 360) ** 2))
+    projectile.simulate(640, 360, 0.001)
     
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(240)  # limits FPS to 240
 
 pygame.quit()
