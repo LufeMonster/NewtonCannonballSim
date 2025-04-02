6af1b83ec9d33c1380b5b23419b7e11a9b154eeb
LufeMonster <lucasmodolo2@gmail.com>
2025-04-02 11:01:43 -0300

    Version 0.5
    Added numpy library for improvment in vector and matrices calculatinons, as such, almost all properties that had separeted components were combined into vectors and matrices. Also declared lots of variables and constants for better readability and ease of complilation, removed acceleration components as they were not needed anymore and corrected zoom calculation.

--- src/simulation.py
+++ src/simulation.py
@@ -1,81 +1,83 @@
 import pygame
 import math
+import numpy as np
 
-SCREEN_RES_X = 1280
-SCREEN_RES_Y = 720
+SCREEN_RES: np.ndarray = np.array([1280, 720], dtype=np.uint16)
 
-GRAVITATIONAL_CONSTANT = 6.6743 * (10 ** -11)
-DENSITY = 5500 # Density constant for mass and gravity force calculation. Expressed in Kg/m³. Earth density is aprox. 5500 Kg/m³.
+GRAVITATIONAL_CONSTANT: float = 6.6743 * (10 ** -11)
+DENSITY: float = 5500 # Density constant for mass and gravity force calculation. Expressed in Kg/m³. Earth density is aprox. 5500 Kg/m³.
+NULL_VECTOR: np.ndarray = np.array([0, 0], dtype=np.uint8)
+UNIT_VECTOR: np.ndarray = np.array([1, 1], dtype=np.uint8)
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_RES_X, SCREEN_RES_Y))
+screen = pygame.display.set_mode((SCREEN_RES[0], SCREEN_RES[1]))
 clock = pygame.time.Clock()
 
 # Class Projectile
 class Projectile:
-    def __init__(self, pos_x, pos_y, vel_x, vel_y, acc_x, acc_y, size, color):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.vel_x = vel_x
-        self.vel_y = vel_y
-        self.acc_x = acc_x
-        self.acc_y = acc_y
-        self.size = size
-        self.color = color
+    def __init__(self, pos: np.ndarray, vel: np.ndarray, size: float, color: tuple):
+        self.moment: np.ndarray = np.array([pos, vel], dtype=np.double)
+        self.size: float = size
+        self.color: tuple = color
     
-    def move(self, dist_x, dist_y):
-        self.pos_x += dist_x
-        self.pos_y += dist_y
+    def move(self, dist: np.ndarray):
+        self.moment += np.array([dist, NULL_VECTOR])
     
-    def accel(self, delta_vel_x, delta_vel_y):
-        self.vel_x += delta_vel_x
-        self.vel_y += delta_vel_y
-    
+    def accel(self, delta_v: np.ndarray):
+        self.moment += np.array([NULL_VECTOR, delta_v])
+        
     def update(self):
-        self.move(self.vel_x, self.vel_y)
-        self.accel(self.acc_x, self.acc_y)
+        self.moment += np.array([self.moment[1], NULL_VECTOR])
         
-    def draw(self, screen_center_x, screen_center_y, zoom):
-        screen_pos_x = self.pos_x * zoom - screen_center_x + (SCREEN_RES_X / 2)
-        screen_pos_y = - self.pos_y * zoom + screen_center_y + (SCREEN_RES_Y / 2)
+    def simulate(self, delta_a: np.ndarray):
+        self.moment += np.array([self.moment[1], delta_a])
         
-        if 0 <= screen_pos_x <= SCREEN_RES_X and 0 <= screen_pos_y <= SCREEN_RES_Y:
-            pygame.draw.circle(screen, self.color, (screen_pos_x, screen_pos_y), self.size * zoom)
+    def draw(self, screen_center: np.ndarray, zoom: float):
+        screen_pos: np.ndarray = np.array([self.moment[0, 0], - self.moment[0, 1]], dtype=np.double)
+        screen_pos -= np.array([screen_center[0], -screen_center[1]])
+        screen_pos *= zoom
+        screen_pos += (SCREEN_RES / 2)
+        screen_pos = screen_pos.astype(dtype=np.int64)
+    
+        if NULL_VECTOR[0] <= screen_pos[0] <= SCREEN_RES[0] and NULL_VECTOR[1] <= screen_pos[1] <= SCREEN_RES[1]:
+            pygame.draw.circle(screen, self.color, screen_pos, self.size * zoom)
 
 class GravityBody:
-    def __init__(self, pos_x, pos_y, diameter, color):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.diameter = diameter
-        self.color = color
-        self.mass = (DENSITY * math.pi * (diameter ** 3)) / 6 # Mass of the planet. Expressed in Kg.
+    def __init__(self, pos: np.ndarray, diameter: float, color: tuple):
+        self.pos: np.ndarray = pos
+        self.diameter: float = diameter
+        self.color: tuple = color
+        self.mass: float = (DENSITY * math.pi * (diameter ** 3)) / 6 # Mass of the planet. Expressed in Kg.
         
-    def draw(self, screen_center_x, screen_center_y, zoom):
-        screen_pos_x = self.pos_x * zoom - screen_center_x + (SCREEN_RES_X / 2)
-        screen_pos_y = - self.pos_y  * zoom + screen_center_y + (SCREEN_RES_Y / 2)
-        if 0 <= screen_pos_x <= SCREEN_RES_X and 0 <= screen_pos_y <= SCREEN_RES_Y:
-            pygame.draw.circle(screen, self.color, (screen_pos_x, screen_pos_y), self.diameter * zoom)
+    def draw(self, screen_center: np.ndarray, zoom: float):
+        screen_pos: np.ndarray = np.array([self.pos[0], -self.pos[1]], dtype=np.double)
+        screen_pos -= np.array([screen_center[0], -screen_center[1]])
+        screen_pos *= zoom
+        screen_pos += (SCREEN_RES / 2)
+        screen_pos = screen_pos.astype(dtype=np.int64)
+    
+        if NULL_VECTOR[0] <= screen_pos[0] <= SCREEN_RES[0] and NULL_VECTOR[1] <= screen_pos[1] <= SCREEN_RES[1]:
+            pygame.draw.circle(screen, self.color, screen_pos, self.diameter * zoom)
         
     def exerce_gravity(self, projectile: Projectile):
-        delta_x = projectile.pos_x - self.pos_x
-        delta_y = projectile.pos_y - self.pos_y
-        
-        distance_from_center_of_mass = math.sqrt(((delta_x) ** 2) + ((delta_y)**2))
+        distance_from_center_of_mass = np.linalg.norm(projectile.moment[0] - self.pos)
         gravity_force = -(GRAVITATIONAL_CONSTANT * self.mass) / distance_from_center_of_mass
         
-        projectile.accel(gravity_force * (delta_x / distance_from_center_of_mass), gravity_force * (delta_y / distance_from_center_of_mass))
-        projectile.update()
-    
-projectile = Projectile(0, 180, 0.5, 0, 0, 0, 8, "red")
+        cos_alpha = (projectile.moment[0, 0] - self.pos[0]) / distance_from_center_of_mass
+        sen_alpha = (projectile.moment[0, 1] - self.pos[1]) / distance_from_center_of_mass
+        
+        projectile.simulate(np.array([gravity_force * cos_alpha, gravity_force * sen_alpha]))
+        
+projectile: Projectile = Projectile(np.array([0, 180]), np.array([0.5, 0]), 8, (255, 0, 0))
 
-planet = GravityBody(0, 0, 100, "blue")
+planet: GravityBody = GravityBody(np.array([0, 0]), 100, (0, 0, 255))
 
-screen_center_x = 0
-screen_center_y = 0
-zoom = 1
+screen_center: np.ndarray = np.array([0, 0], dtype=np.double)
+zoom: float = 1
 
-running = True
+running: bool = True
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -85,13 +87,13 @@ while running:
     
     key = pygame.key.get_pressed()
     if key[pygame.K_UP] == True:
-        screen_center_y += 1
+        screen_center[1] += (1 / zoom)
     elif key[pygame.K_RIGHT] == True:
-        screen_center_x += 1
+        screen_center[0] += (1 / zoom)
     elif key[pygame.K_DOWN] == True:
-        screen_center_y -= 1
+        screen_center[1] -= (1 / zoom)
     elif key[pygame.K_LEFT] == True:
-        screen_center_x -= 1
+        screen_center[0] -= (1 / zoom)
     elif key[pygame.K_KP_PLUS] == True:
         zoom += 0.01
     elif key[pygame.K_KP_MINUS] == True:
@@ -102,10 +104,11 @@ while running:
 
     # RENDER YOUR GAME HERE
     
-    planet.draw(screen_center_x, screen_center_y, zoom)
+    planet.draw(screen_center, zoom)
     planet.exerce_gravity(projectile)
 
-    projectile.draw(screen_center_x, screen_center_y, zoom)
+    projectile.draw(screen_center, zoom)
+    # print("pos_x:", projectile.pos_x, "pos_y:", projectile.pos_y)
     
     # flip() the display to put your work on screen
     pygame.display.flip()
