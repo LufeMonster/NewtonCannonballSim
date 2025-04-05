516cb5575df63d027b77da9f4033fd270fd014f4
LufeMonster <lucasmodolo2@gmail.com>
2025-04-05 13:27:31 -0300

    Version 0.7
    Improved during-run controls, like being able to control cannon angle and fire speed. Also now is possible to create multiple projectiles and added in-simulation text to track angle, velocity and last key pressed.

--- src/simulation.py
+++ src/simulation.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame.freetype
 import math
 import numpy as np
 
@@ -12,6 +13,7 @@ UNIT_VECTOR: np.ndarray = np.array([1, 1], dtype=np.uint8)
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_RES[0], SCREEN_RES[1]))
+SIM_FONT = pygame.freetype.SysFont(pygame.freetype.get_default_font(), 32)
 clock = pygame.time.Clock()
 
 # class Projectile
@@ -82,11 +84,12 @@ class GravityBody:
 
 # class Cannon
 class Cannon:
-    def __init__(self, pos: np.ndarray, gravity_body: GravityBody, angle: float, firing_speed: np.ndarray):
+    def __init__(self, pos: np.ndarray, gravity_body: GravityBody, angle: float, firing_speed_modulus: float):
         self.pos: np.ndarray = pos + gravity_body.pos
         self.gravity_body: GravityBody = gravity_body
         self.angle: float = angle
-        self.firing_speed: np.ndarray = firing_speed
+        self.firing_speed_modulus: float = firing_speed_modulus
+        self.firing_speed = np.array([firing_speed_modulus * math.cos(math.radians(self.angle)), firing_speed_modulus * math.sin(math.radians(self.angle))])
         
     def get_screen_pos(self,screen_center: np.ndarray, zoom: float):
         screen_pos: np.ndarray = np.array([self.pos[0], -self.pos[1]], dtype=np.double)
@@ -105,22 +108,22 @@ class Cannon:
         cannon_sprite: pygame.Rect = pygame.Rect(screen_pos_cannon[0] - TAM, screen_pos_cannon[1] - TAM, TAM * 2, TAM * 2)
         pygame.draw.line(screen, (0, 255, 0), screen_pos_gravity_body, screen_pos_cannon, int(8 * zoom))
         pygame.draw.rect(screen, (0, 255, 0), cannon_sprite.scale_by(zoom))
-    
-    def fire(self, angle: float, firing_speed: float, projectile_size: float, projectile_color: tuple):
-        self.angle = angle
-        self.firing_speed = np.array([firing_speed * math.cos(self.angle), firing_speed * math.sin(self.angle)])
         
+    def fire(self, projectile_size: float, projectile_color: tuple):
+        self.firing_speed = np.array([self.firing_speed_modulus * math.cos(math.radians(self.angle)), self.firing_speed_modulus * math.sin(math.radians(self.angle))])
         projectile: Projectile = Projectile(np.array([self.pos[0], self.pos[1]]), np.array([self.firing_speed[0], self.firing_speed[1]]), projectile_size, projectile_color)
         return projectile
 
 planet: GravityBody = GravityBody(np.array([0, 0]), 100, (0, 0, 255))
-cannon: Cannon = Cannon(np.array([0, 180]), planet, 0, np.array([0, 0]))
+cannon: Cannon = Cannon(np.array([0, 180]), planet, 0, 0.44)
+projectiles = []
 
 screen_center: np.ndarray = np.array([0, 0], dtype=np.double)
 zoom: float = 1
 
 running: bool = True
-created: bool = False
+
+last_key_pressed = "NONE"
 
 while running:
     # poll for events
@@ -128,23 +131,44 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                projectiles.append(cannon.fire(8, (255, 0, 0)))
+                last_key_pressed = "SPACE"
+    
     
     key = pygame.key.get_pressed()
-    if key[pygame.K_UP] == True:
+    if key[pygame.K_w] == True:
         screen_center[1] += (1 / zoom)
-    elif key[pygame.K_RIGHT] == True:
+        last_key_pressed = "w"
+    elif key[pygame.K_d] == True:
         screen_center[0] += (1 / zoom)
-    elif key[pygame.K_DOWN] == True:
+        last_key_pressed = "d"
+    elif key[pygame.K_s] == True:
         screen_center[1] -= (1 / zoom)
-    elif key[pygame.K_LEFT] == True:
+        last_key_pressed = "s"
+    elif key[pygame.K_a] == True:
         screen_center[0] -= (1 / zoom)
+        last_key_pressed = "a"
     elif key[pygame.K_KP_PLUS] == True:
         zoom += 0.01
+        last_key_pressed = "+"
     elif key[pygame.K_KP_MINUS] == True:
         zoom -= 0.01
-    elif key[pygame.K_SPACE] == True:
-        projectile: Projectile = cannon.fire(math.radians(0), 0.44, 8, (255, 0, 0))
-        created = True
+        last_key_pressed = "-"
+        
+    elif key[pygame.K_UP] == True:
+        cannon.angle += 0.1
+        last_key_pressed = "UP"
+    elif key[pygame.K_DOWN] == True:
+        cannon.angle -= 0.1
+        last_key_pressed = "DOWN"
+    elif key[pygame.K_RIGHT] == True:
+        cannon.firing_speed_modulus += 0.001
+        last_key_pressed = "RIGHT"
+    elif key[pygame.K_LEFT] == True:
+        cannon.firing_speed_modulus -= 0.001
+        last_key_pressed = "LEFT"
         
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
@@ -154,9 +178,18 @@ while running:
     cannon.draw(screen_center, zoom)
     planet.draw(screen_center, zoom)
     
-    if created:
+    for projectile in projectiles:
         planet.exerce_gravity(projectile)
         projectile.draw(screen_center, zoom)
+        
+    angle_display_txt = "Angle: {angle:.2f}°"
+    velocity_display_txt = "Velocity: {speed: .2f} m/s"
+    
+    SIM_FONT.render_to(screen, (0, 0), angle_display_txt.format(angle = cannon.angle), (255, 255, 255))
+    SIM_FONT.render_to(screen, (0, 32), velocity_display_txt.format(speed = cannon.firing_speed_modulus), (255, 255, 255))
+    
+    SIM_FONT.render_to(screen, (0, 64), "Last key pressed: " + last_key_pressed, (255, 255, 255))
+    
     
     # flip() the display to put your work on screen
     pygame.display.flip()
